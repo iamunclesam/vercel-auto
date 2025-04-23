9ab37452fd4736839089bc6b8115bd597466e35c
Code-Gale <codewhiz001@gmail.com>
2025-04-23 21:03:46 +0100

    :main entry app

---
+++ app.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const connectDB = require('./config/db');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const http = require('http');
+const deployRoutes = require('./routes/deployRoutes');
+const domainRoutes = require('./routes/domainRoutes');
+const usageRoutes = require('./routes/usageRoutes');
+
+
+//starting app
+const app = express();
+const server = http.createServer(app);
+
+//middlewares
+app.use(cors({
+  origin: "*", 
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true  
+}));
+
+app.use('/api/deploy', deployRoutes);
+app.use('/api/domain', domainRoutes);
+app.use('/api/usage', usageRoutes);
+
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+app.set('trust proxy', false);
+
+
+app.get('/', (req, res) => {
+  res.status(200).json('Welcome to Vercel Theme Deployer');
+});
+
+const PORT = process.env.PORT || 5050;
+
+app.listen(PORT, () => {
+  console.log('Listening for requests on', PORT);
+});
+
+module.exports = app;
